ce52f2552e8133eecbb4c05a22d06f3e617cf0a2
Guilherme Souza <sitegui@sitegui.com.br>
2013-10-05 16:21:09 -0300

    Corrige bug de foco no Firefox

--- js/interface/JanelaDownload.js
+++ js/interface/JanelaDownload.js
@@ -10,9 +10,11 @@ JanelaDownload.init = function () {
 JanelaDownload.onabrir = function (opcoes) {
 	get("janelaDownload-link").href = opcoes.url
 	get("janelaDownload-link").download = opcoes.nome
+	get("edicao").contentEditable = "false" // Bug do firefox: perda de foco ao clicar em download
 	get("janelaDownload-cancelar").onclick = get("janelaDownload-fechar").onclick = function () {
 		Interface.fecharJanela()
 		URL.revokeObjectURL(opcoes.url)
 		JanelaDicas.disparar("acao", "offline")
+		get("edicao").contentEditable = "true"
 	}
 }
